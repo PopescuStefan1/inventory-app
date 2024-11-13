9b050971689165f80d43c95d8470e6cc5c435ae1
PopescuStefan1 <stefan.ro95@gmail.com>
2024-11-13 21:55:04 +0200

    Add comments

--- db/pool.js
+++ db/pool.js
@@ -2,8 +2,8 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export default new Pool({
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
+  user: process.env.USERNAME, // Add your role_name here
+  password: process.env.PASSWORD, // Add your role_password to here
   host: "localhost",
   database: "inventory_management",
   port: 5432,
